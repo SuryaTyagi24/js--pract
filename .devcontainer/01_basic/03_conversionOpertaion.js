34e849044bffa60cba6596d6472b4dacf57964b5
Suryadev Tyagi <105010381+SuryaTyagi24@users.noreply.github.com>
2024-09-25 10:00:43 +0000

    conversion

--- .devcontainer/01_basic/03_conversionOpertaion.js
+++ .devcontainer/01_basic/03_conversionOpertaion.js
@@ -7,4 +7,7 @@ let valueInNumber = Number(score);
 console.log(typeof valueInNumber);
 console.log(valueInNumber);
 
+let isLoggedIn =1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
 
