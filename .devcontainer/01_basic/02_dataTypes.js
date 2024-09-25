3420bf061198e5752dbe2f1b9a2a791006cbfa5a
Suryadev Tyagi <105010381+SuryaTyagi24@users.noreply.github.com>
2024-09-25 09:28:47 +0000

    datatype

---
+++ .devcontainer/01_basic/02_dataTypes.js
@@ -0,0 +1,15 @@
+let name = "Tyagi"
+let age = 21
+let isLoggedIn =false
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+// undefined =>
+// symbol => unique
+
+//object
+
+console.log(typeof "Tyagi");
