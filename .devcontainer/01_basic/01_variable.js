35926730a8288d11b0e2e70fb7fc4b1019cf81ac
Suryadev Tyagi <105010381+SuryaTyagi24@users.noreply.github.com>
2024-09-25 09:28:27 +0000

    variable

---
+++ .devcontainer/01_basic/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 14637
+let accountEmail = "tyagidivyam24@gmail.com"
+var accountPassword = "1435"
+accountCity = "Noida"
+
+accountEmail = "st@ts.com"
+accountPassword = "1445"
+accountCity = "Greater Noida"
+
+console.log(accountId);
+console.table([accountEmail, accountPassword , accountCity])
+
+/*
+  prefer Not to use var
+*/
