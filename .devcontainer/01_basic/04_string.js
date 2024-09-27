7135356b2b32b5005c8e142c890010764aaab8ee
Suryadev Tyagi <105010381+SuryaTyagi24@users.noreply.github.com>
2024-09-27 10:22:23 +0000

    commit

---
+++ .devcontainer/01_basic/04_string.js
@@ -0,0 +1,25 @@
+ const name = "Tyagi"
+ const repoCount = 50
+
+ //console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+ const gameName = new String(`tyagist`)
+
+ //console.log(gameName[0]);
+ //console.log(gameName.__proto__);
+
+ //console.log(gameName.length);
+ //console.log(gameName.toUpperCase());
+ //console.log(gameName.charAt('t'));
+ 
+ //const anotherString = gameName.slice(0, 4)
+ //console.log(anotherString);
+
+ const url = " https://tyagidivyam.com/Surya%24Tyagi"
+
+ console.log(url.replace('%24', '-'))
+ console.log(url.includes('tyagi'));
+ 
+ console.log(gameName.split('-'))
+ 
+ 
