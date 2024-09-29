98268bff0acce36a891439ad02c405110e321161
Suryadev Tyagi <105010381+SuryaTyagi24@users.noreply.github.com>
2024-09-29 10:00:31 +0000

    c

---
+++ .devcontainer/02_basic/03_object.js
@@ -0,0 +1,14 @@
+// singleton made from constructor
+//objects.create - method for constructor
+
+
+//objects literals
+const JsUser ={
+    name: "Tyagi",
+    age: 21,
+    location:  "Noida",
+    email: "tyagi21@gmail.com",
+    isLoggedIn : false,
+    lastLoginDays : ["Mon", "Sat"]
+}
+console.log(JsUser.email);
