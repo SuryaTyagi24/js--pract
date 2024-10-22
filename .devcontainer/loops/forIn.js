269dbfb5a62160694135d420c8cf997863b956df
Suryadev Tyagi <105010381+SuryaTyagi24@users.noreply.github.com>
2024-10-22 09:40:46 +0000

    for in loop or Each loop

---
+++ .devcontainer/loops/forIn.js
@@ -0,0 +1,25 @@
+//For In
+
+//const programming = ["js", "rb", "py", "java","cpp"]
+// for(const key in programming){
+//    // console.log(key); only for key value
+//    //console.log(programming[key]);
+   
+// }
+
+// For Each LOOP
+const programming = ["js", "rb", "py", "java","cpp"]
+
+// programming.forEach(function(val){
+//     console.log(val);
+    
+// })
+
+programming.forEach((item) => {
+    console.log(item);
+    
+// })
+
+// programming.forEach((item, index,arr)=> {
+//     console.log(item,index,arr);   
+// })
