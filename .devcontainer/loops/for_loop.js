49aef6e12d213b2decdc5fdffbf9bdb615bed93f
Suryadev Tyagi <105010381+SuryaTyagi24@users.noreply.github.com>
2024-10-22 08:51:50 +0000

    loops

---
+++ .devcontainer/loops/for_loop.js
@@ -0,0 +1,25 @@
+//For
+
+for(let index =0 ; index<=10; index++){
+    const element = 11;
+    //console.log(element);
+    
+}
+// while loop
+// while(condition){
+
+// }
+
+let index = 0
+while (index <=10) {
+    //console.log(`Value of index is ${index}`);
+    index = index + 2
+}
+
+//Do- while loop
+
+let score =1
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <=10);
