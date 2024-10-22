6d512c99df45a673d44a7ec932d459eca492baab
Suryadev Tyagi <105010381+SuryaTyagi24@users.noreply.github.com>
2024-10-22 08:06:59 +0000

    controlFlow

---
+++ .devcontainer/04_control_flow.js/one.js
@@ -0,0 +1,25 @@
+//iF
+
+const isUserloggedIn = true
+
+if(isUserloggedIn){
+
+}
+//2<=2 , 3!=2
+//<,>, <=, >=, ==, != , !== :
+// === : it also check the type 
+
+const temp = 40
+if(temp < 50){
+    //console.log("temp  is 50");    
+}
+
+const score = 300
+if(score > 100){
+    let power = "fly"
+    //console.log(`User power: ${power}`);
+}
+const balance = 1000
+//if (balance > 500) console.log("test");  <--- implisite scope
+
+
